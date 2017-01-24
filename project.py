73abf234aaa003caa91d1257d951d848bb1baf3d
sonnenfeld269 <david.sonnenfeld269@gmail.com>
2017-01-24 07:21:05 +0100

    implement routing, template-engine, all-restaurants and show-restaurant use case

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 from database_service import *
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
@@ -6,18 +6,47 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 
-@app.route('/')
+
 @app.route('/hello')
 def HelloWorld():
     return "HelloWorld"
-
-@app.route('/restaurants')
+    
+@app.route('/')
+@app.route('/restaurants/')
 def showAllRestaurants():
-    result = readAllRestaurants();
-    output = ""
-    for r in result:
-        output += r.name + "</br></br>"
-    return output
+    result = readAllRestaurants()
+    print "returning" + str(result)
+    return render_template("restaurants.html", restaurants = result)
+
+@app.route('/restaurants/<int:restaurant_id>')
+def showRestaurant(restaurant_id):
+    print "inside show restaurant"
+    print restaurant_id
+    result = readRestaurantById(restaurant_id)
+    menuItems = readMenuItemsByRestaurant(restaurant_id)
+    return render_template("restaurant.html", restaurant = result, menuItems = menuItems)
+
+@app.route('/restaurants/new', methods=['GET','POST'])
+def newRestaurant():
+    if request.method == "POST":
+        restaurant = createRestaurant(request.form['name'])
+        return redirect(url_for('showRestaurant',restaurant_id=restaurant.id))
+    else:
+        return render_template("add_restaurant.html")
+
+"""
+@app.route('/restaurants/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+    return "new"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "edit"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "delete"
+"""
 
 def dbSetup(dbtype,dbname):
     engine = create_engine('%s:///%s' % (dbtype,dbname))
