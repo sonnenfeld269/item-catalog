cd85e27929de1a842183450e2e6190c4868a5dfd
sonnenfeld269 <david.sonnenfeld269@gmail.com>
2017-02-07 13:50:05 +0100

    improve jquery calls, style form css  and show active tabs correctly

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,24 +1,12 @@
 $(document).ready(function() {
     /* Add item */
-    $(".input-section").hide();
-    $(".cancel-button").hide();
-    $(".save-button").hide();
+    $(".add-form").hide();
     $(".add-button").click(function() {
         $(this).hide();
-        $(".cancel-button").show();
-        $(".save-button").show();
-        $(this).parent().parent().parent().find(".input-section").toggle();
+        $(".add-form").toggle();
     });
     $(".cancel-button").click(function() {
-        $(this).hide();
-        $(".save-button").hide();
         $(".add-button").show();
-        $(this).parent().parent().parent().find(".input-section").toggle();
-    });
-
-    /* Add active class to category list */
-    $('.nav').on('click', 'li', function(){
-        $('.nav li').removeClass('active');
-        $(this).addClass('active');
+        $(".add-form").toggle();
     });
 });
