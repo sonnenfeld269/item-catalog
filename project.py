d6fee5fc8df975cd2cf034c5be6a84f6df5b9185
sonnenfeld269 <david.sonnenfeld269@gmail.com>
2017-02-07 08:49:41 +0100

    fix delete issue with items and use pep8

--- project.py
+++ project.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-#### User Actions
+# User Actions
+
 
 @app.route('/')
 @app.route('/items')
@@ -18,16 +19,15 @@ def showAllData():
     num_items = len(items)
 
     rendered_categories = render_template("category/show_all_categories.html",
-                                           categories = categories)
+                                          categories=categories)
 
-    rendered_items = render_template("item/show_items.html", items = items,
-                                      categories = categories,
-                                      countItems = num_items)
+    rendered_items = render_template("item/show_items.html", items=items,
+                                     categories=categories,
+                                     countItems=num_items)
 
     return render_template("display_all.html",
-                            categories = rendered_categories,
-                            items = rendered_items)
-
+                           categories=rendered_categories,
+                           items=rendered_items)
 
 
 @app.route('/<string:category_name>/items')
@@ -40,44 +40,48 @@ def showDataByCategory(category_name):
     num_items = len(items)
 
     rendered_categories = render_template("category/show_all_categories.html",
-                                           categories = categories,
-                                           category_name = category_name)
+                                          categories=categories,
+                                          category_name=category_name)
 
     rendered_items = render_template("item/show_items.html",
-                                      items = items, categories = categories,
-                                      category_name = category_name,
-                                      countItems = num_items)
+                                     items=items, categories=categories,
+                                     category_name=category_name,
+                                     countItems=num_items)
 
     return render_template("display_all.html",
-                            categories = rendered_categories,
-                            items = rendered_items)
+                           categories=rendered_categories,
+                           items=rendered_items)
+
 
 @app.route('/items/new', methods=['POST'])
 @app.route('/<string:category_name>/items/new', methods=['POST'])
 def addItem(category_name=None):
 
-        if request.method == "POST":
-            if not category_name:
-                category_name = request.form['selected_category_name']
+    if request.method == "POST":
+        if not category_name:
+            category_name = request.form['selected_category_name']
 
-            category_id=readCategoryByName(category_name).id
-            item = createItem(request.form['title'],request.form['description'],category_id)
-            flash("Item successfully added!", "success")
-            return redirect(url_for('showDataByCategory',category_name=category_name))
+        category_id = readCategoryByName(category_name).id
+        item = createItem(request.form['title'], request.form[
+                          'description'], category_id)
+        flash("Item successfully added!", "success")
+        return redirect(url_for('showDataByCategory', category_name=category_name))
 
-@app.route('/items/<int:item_id>/edit', methods=['GET','POST'])
-@app.route('/<string:category_name>/items/<int:item_id>/edit', methods=['GET','POST'])
+
+@app.route('/items/<int:item_id>/edit', methods=['GET', 'POST'])
+@app.route('/<string:category_name>/items/<int:item_id>/edit', methods=['GET', 'POST'])
 def editItem(category_name=None, item_id=None):
     category_name = ""
     if request.method == "GET":
         item = readItemById(item_id)
         if not category_name:
             category_name = readCategoryById(item.category_id).name
-        return render_template('item/edit_item.html', item = item)
+        return render_template('item/edit_item.html', item=item)
     else:
         updateItem(item_id, request.form['title'], request.form['description'])
         flash("Item successfully edited!", "success")
-        return redirect(url_for('showDataByCategory',category_name=category_name))
+        return redirect(url_for('showDataByCategory', category_name=category_name))
+
 
 @app.route('/items/<int:item_id>/delete', methods=['POST'])
 @app.route('/<string:category_name>/items/<int:item_id>/delete', methods=['POST'])
@@ -89,9 +93,12 @@ def removeItem(category_name=None, item_id=None):
     flash("Item successfully deleted!", "success")
 
     # redirect to showDataByCategory
-    return redirect(url_for('showDataByCategory',category_name=category_name))
+    if category_name:
+        return redirect(url_for('showDataByCategory', category_name=category_name))
+    else:
+        return redirect(url_for('showAllData'))
+# Category - API Endpoints
 
-#### Category - API Endpoints
 
 @app.route('/categories/JSON')
 def getAllCategoriesJSON():
@@ -100,15 +107,17 @@ def getAllCategoriesJSON():
     categories = readAllCategories()
     return jsonify(Categories=[i.serialize for i in categories])
 
+
 @app.route('/categories/<string:category_name>/JSON')
 def getCategoryByNameJSON(category_name):
     category = readCategoryByName(category_name)
     return jsonify(Category=category.serialize)
 
-#### Item - API Endpoints
+# Item - API Endpoints
+
 
 @app.route('/<string:category_name>/items/JSON')
-def getAllItemsJSON(category_name = None):
+def getAllItemsJSON(category_name=None):
     """Returns all items as json.
 
     Args:
@@ -125,17 +134,15 @@ def getAllItemsJSON(category_name = None):
     return jsonify(Items=[i.serialize for i in items])
 
 
+# Configuration
 
-
-#### Configuration
-
-def dbSetup(dbtype,dbname):
-    engine = create_engine('%s:///%s' % (dbtype,dbname))
+def dbSetup(dbtype, dbname):
+    engine = create_engine('%s:///%s' % (dbtype, dbname))
     Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
-dbSetup("sqlite","category_item.db")
+dbSetup("sqlite", "category_item.db")
 
 if __name__ == '__main__':
     app.secret_key = "my_secret_key"
